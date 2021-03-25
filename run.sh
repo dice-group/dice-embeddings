daa37c366105b0108c2452b8825e7ccb26f45d87
Caglar Demir <caglardemir8@gmail.com>
2021-03-25 14:04:51 +0100

    gpu command added.

--- run.sh
+++ run.sh
@@ -2,22 +2,22 @@
 python --version
 python -u -c 'import torch; print(torch.__version__)'
 echo "Start Training......"
-python main.py --path_dataset_folder 'KGs/Animals' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Biopax' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Forte' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Carcinogenesis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Hepatitis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Lymphography' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Mammographic' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Nctrer' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
-python main.py --path_dataset_folder 'KGs/Mutagenesis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Animals' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Biopax' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Forte' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Carcinogenesis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Hepatitis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Lymphography' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Mammographic' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Nctrer' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
+python main.py --gpus 1 --path_dataset_folder 'KGs/Mutagenesis' --model 'ConEx' --max_num_epochs 100 --embedding_dim 25
 
-python main.py --path_dataset_folder 'KGs/Animals' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Biopax' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Carcinogenesis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Forte' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Hepatitis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Lymphography' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Mammographic' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Mutagenesis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
-python main.py --path_dataset_folder 'KGs/Nctrer' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Animals' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Biopax' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Carcinogenesis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Forte' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Hepatitis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Lymphography' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Mammographic' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Mutagenesis' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
+python main.py --gpus 1 --path_dataset_folder 'KGs/Nctrer' --model 'Shallom' --max_num_epochs 100 --embedding_dim 50
