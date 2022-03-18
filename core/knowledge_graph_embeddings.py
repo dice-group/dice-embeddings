91b6bd0839028900bba595accbda94ab7cb2eb1c
Caglar Demir <caglardemir8@gmail.com>
2022-03-18 09:19:01 +0100

    KvsAll training is removed from interactive setting as it requires input KG to be in memory.This idea of using KvsAll in continual learning will be revised.

--- core/knowledge_graph_embeddings.py
+++ core/knowledge_graph_embeddings.py
@@ -15,7 +15,7 @@ class KGE(BaseInteractiveKGE):
     def __init__(self, path_of_pretrained_model_dir, construct_ensemble=False, model_path=None):
         super().__init__(path_of_pretrained_model_dir, construct_ensemble=construct_ensemble, model_path=model_path)
         self.is_model_in_train_mode = False
-        # Workaround due to BN output
+
     def predict_topk(self, *, head_entity: list = None, relation: list = None, tail_entity: list = None,
                      k: int = 10) -> Generator:
         """
@@ -61,12 +61,7 @@ class KGE(BaseInteractiveKGE):
                                                                                                      1)
         x = torch.hstack((head_entity, relation, tail_entity))
         with torch.no_grad():
-            return self.model.forward_triples_multiply(x)
-
-    def indexed_triple_score(self, i):
-        i = torch.LongTensor(i).reshape(1, 3)
-        with torch.no_grad():
-            return torch.sigmoid(self.model(torch.LongTensor(i)))
+            return torch.sigmoid(self.model.forward_triples_multiply(x))
 
     def train(self, kg, lr=.1, epoch=10, batch_size=32, neg_sample_ratio=10, num_workers=1) -> None:
         # (1) Create Negative Sampling Setting for training
@@ -75,6 +70,7 @@ class KGE(BaseInteractiveKGE):
                                             num_entities=len(kg.entity_to_idx),
                                             num_relations=len(kg.relation_to_idx),
                                             neg_sample_ratio=neg_sample_ratio)
+        print('Number of data points: ', len(train_set))
         train_dataloader = DataLoader(train_set, batch_size=batch_size,
                                       shuffle=True, num_workers=num_workers,
                                       collate_fn=train_set.collate_fn, pin_memory=True)
@@ -112,7 +108,7 @@ class KGE(BaseInteractiveKGE):
             pred = self.model(x)
             last_avg_loss_per_triple += self.model.loss(pred, y)
         last_avg_loss_per_triple /= len(train_set)
-        print(f'On average Improvement: {first_avg_loss_per_triple - last_avg_loss_per_triple}:.3f')
+        print(f'On average Improvement: {first_avg_loss_per_triple - last_avg_loss_per_triple:.3f}')
 
     def train_triples(self, head_entity, relation, tail_entity, labels, iteration=2, lr=.1, repeat=2):
 
@@ -144,70 +140,4 @@ class KGE(BaseInteractiveKGE):
         with torch.no_grad():
             outputs = self.model.forward_triples_multiply(x)
             loss = self.model.loss(outputs, labels)
-        print(f"Eval Mode:Loss:{loss.item():.4f}\t Outputs:{outputs.detach()}")
-
-    def train_triples_lbfgs(self, head_entity, relation, tail_entity, labels, iteration=100):
-
-        assert len(head_entity) == len(relation) == len(tail_entity) == len(labels)
-        n = len(head_entity)
-
-        head_entity = torch.LongTensor(self.entity_to_idx.loc[head_entity]['entity'].values).reshape(n, 1)
-        relation = torch.LongTensor(self.relation_to_idx.loc[relation]['relation'].values).reshape(n, 1)
-        tail_entity = torch.LongTensor(self.entity_to_idx.loc[tail_entity]['entity'].values).reshape(n, 1)
-
-        x = torch.hstack((head_entity, relation, tail_entity))
-        labels: object = torch.FloatTensor(labels)
-
-        if n == 1:
-            x = x.repeat(2, 1)
-            labels = labels.repeat(2)
-
-        self.set_model_train_mode()
-        optimizer = optim.LBFGS(self.model.parameters())
-
-        for epoch in range(iteration):  # loop over the dataset multiple times
-            def closure():
-                optimizer.zero_grad()
-                outputs = self.model(x)
-                loss = self.model.loss(outputs, labels)
-                loss.backward()
-                return loss
-
-            # Take step.
-            optimizer.step(closure)
-
-        self.set_model_eval_mode()
-
-    def train_vs_all(self, head_entity, relation, tail_entity, iteration=1, lr=.01):
-        raise NotImplementedError('Undesired')
-        self.set_model_eval_mode()
-        assert len(head_entity) == len(relation) == len(tail_entity)
-        n = len(head_entity)
-
-        head_entity = torch.LongTensor(self.entity_to_idx.loc[head_entity]['entity'].values).reshape(n, 1)
-        relation = torch.LongTensor(self.relation_to_idx.loc[relation]['relation'].values).reshape(n, 1)
-        tail_entity = torch.LongTensor(self.entity_to_idx.loc[tail_entity]['entity'].values).reshape(n, 1)
-
-        x = torch.hstack((head_entity, relation))
-
-        outputs = self.model(x)
-        y = torch.zeros(outputs.shape)
-
-        y[:, tail_entity] = 1
-
-        if n == 1:
-            x = x.repeat(2, 1)
-            y = y.repeat(2, 1)
-
-        self.set_model_train_mode()
-        optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        for epoch in range(iteration):  # loop over the dataset multiple times
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            # forward + backward + optimize
-            outputs = self.model(x)
-            loss = self.model.loss(outputs, y)
-            loss.backward()
-            optimizer.step()
-
-        self.set_model_eval_mode()
+        print(f"Eval Mode:Loss:{loss.item():.4f}\t Outputs:{torch.sigmoid(outputs).detach()}")
