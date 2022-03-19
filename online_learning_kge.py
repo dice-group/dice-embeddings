c036f03ff85a556d2ba88dfe3682c4e987fca4c8
Caglar Demir <caglardemir8@gmail.com>
2022-03-19 16:42:26 +0100

    type query dded.

--- online_learning_kge.py
+++ online_learning_kge.py
@@ -1,21 +1,35 @@
 from core import KGE
 from core.knowledge_graph import KG
 import torch
-pre_trained_kge = KGE(path_of_pretrained_model_dir='DBpediaQMult')
+
+# pre_trained_kge = KGE(path_of_pretrained_model_dir='DBpediaQMult')
+pre_trained_kge = KGE(path_of_pretrained_model_dir='Experiments/2022-03-19 16:40:54.529443')
 persons = [["http://dbpedia.org/resource/Albert_Einstein"], ["http://dbpedia.org/resource/John_von_Neumann"],
-           ["http://dbpedia.org/resource/Stephen_Hawking"]]
+           ["http://dbpedia.org/resource/Stephen_Hawking"], ["http://dbpedia.org/resource/Germany"],
+           ["http://dbpedia.org/resource/Turkey"]]
+
+num_iter = 50
+repeat = 10
 for p in persons:
     # 10 times p r 110MillionEntities => 110 10^7 data points.
-    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/citizenship"], iteration=100,
-                                   repeat=10, lr=.1)
-    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/nationality"], iteration=100,
-                                   repeat=10, lr=.1)
-    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/birthPlace"], iteration=100,
-                                   repeat=10, lr=.1)
-    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/deathPlace"], iteration=100,
-                                   repeat=10, lr=.1)
-    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/knownFor"], iteration=100,
-                                   repeat=10, lr=.1)
+
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"],
+                                   iteration=num_iter,
+                                   repeat=repeat, lr=.1)
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/citizenship"],
+                                   iteration=num_iter,
+                                   repeat=repeat, lr=.1)
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/nationality"],
+                                   iteration=num_iter,
+                                   repeat=repeat, lr=.1)
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/birthPlace"],
+                                   iteration=num_iter,
+                                   repeat=repeat, lr=.1)
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/deathPlace"],
+                                   iteration=num_iter,
+                                   repeat=repeat, lr=.1)
+    pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/knownFor"], iteration=num_iter,
+                                   repeat=repeat, lr=.1)
     pre_trained_kge.train_k_vs_all(head_entity=p, relation=["http://dbpedia.org/ontology/wikiPageWikiLink"],
-                                   iteration=100, repeat=10, lr=.1)
+                                   iteration=num_iter, repeat=repeat, lr=.1)
 pre_trained_kge.save()
