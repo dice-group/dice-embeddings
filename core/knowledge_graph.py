a3f2bb671e3b8a54ede7d9d8f2850a8dcfe32f62
Caglar Demir <caglardemir8@gmail.com>
2022-04-06 11:22:12 +0200

    domain and range constraints per relations are explicitly stored in eval mode.

--- core/knowledge_graph.py
+++ core/knowledge_graph.py
@@ -9,7 +9,8 @@ from dask import dataframe as ddf
 import os
 import pandas as pd
 from .static_funcs import performance_debugger, get_er_vocab, get_ee_vocab, get_re_vocab, \
-    create_recipriocal_triples_from_dask, add_noisy_triples, index_triples, load_data_parallel, numpy_data_type_changer
+    create_recipriocal_triples_from_dask, add_noisy_triples, index_triples, load_data_parallel, create_constraints, \
+    numpy_data_type_changer
 from .sanity_checkers import dataset_sanity_checking
 import glob
 
@@ -193,6 +194,7 @@ class KG:
                 self.re_vocab = get_re_vocab(data)
                 # 17. Create a bijection mapping from subject-object pairs to relations.
                 self.ee_vocab = get_ee_vocab(data)
+                self.domain_constraints_per_rel, self.range_constraints_per_rel = create_constraints(self.train_set)
         else:
             self.deserialize(deserialize_flag)
 
@@ -208,6 +210,7 @@ class KG:
                 self.re_vocab = get_re_vocab(data)
                 # 17. Create a bijection mapping from subject-object pairs to relations.
                 self.ee_vocab = get_ee_vocab(data)
+                self.domain_constraints_per_rel, self.range_constraints_per_rel = create_constraints(self.train_set)
                 print(f'Done !\t{time.time() - start_time:.3f} seconds\n')
 
         # 4. Display info
@@ -217,7 +220,6 @@ class KG:
                                      f'\nNumber of triples on train set: {len(self.train_set)}' \
                                      f'\nNumber of triples on valid set: {len(self.valid_set) if self.valid_set is not None else 0}' \
                                      f'\nNumber of triples on test set: {len(self.test_set) if self.test_set is not None else 0}\n'
-
 
     def sequential_vocabulary_construction(self):
         print('Train set compute...')
