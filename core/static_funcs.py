17325ab0a36cf097c9719d03ec69cace2e4b0794
Caglar Demir <caglardemir8@gmail.com>
2022-04-06 11:21:28 +0200

    int8 int16 or int32 is used to index entities and relations

--- core/static_funcs.py
+++ core/static_funcs.py
@@ -121,8 +121,7 @@ def load_model_ensemble(path_of_experiment_folder) -> Tuple[BaseKGE, pd.DataFram
 
 
 def numpy_data_type_changer(train_set, num):
-    train_set = train_set.astype(np.int32)
-    return train_set
+    # train_set = train_set.astype(np.int32)
     if np.iinfo(np.int8).max > num:
         print(f'Setting int8,\t {np.iinfo(np.int8).max}')
         train_set = train_set.astype(np.int8)
@@ -412,8 +411,33 @@ def preprocesses_input_args(arg):
         arg.eval = True
     if arg.model == 'Shallom':
         arg.scoring_technique = 'KvsAll'
-    assert arg.normalization in ['LayerNorm','BatchNorm1d']
+    assert arg.normalization in ['LayerNorm', 'BatchNorm1d']
     return arg
+
+
+def create_constraints(triples):
+    """
+    Crete constrainted entities based on the range of relations
+    :param triples:
+    :return:
+    """
+    # (1) Compute the range and domain of each relation
+    range_constraints_per_rel = dict()
+    domain_constraints_per_rel = dict()
+    set_of_entities = set()
+    set_of_relations=set()
+    for (e1, p, e2) in triples:
+        range_constraints_per_rel.setdefault(p, set()).add(e2)
+        domain_constraints_per_rel.setdefault(p, set()).add(e1)
+        set_of_entities.add(e1)
+        set_of_relations.add(p)
+        set_of_entities.add(e2)
+
+    for rel in set_of_relations:
+        range_constraints_per_rel[rel] = list(set_of_entities - range_constraints_per_rel[rel])
+        domain_constraints_per_rel[rel] = list(set_of_entities - domain_constraints_per_rel[rel])
+
+    return domain_constraints_per_rel,range_constraints_per_rel
 
 
 def create_logger(*, name, p):
@@ -537,6 +561,7 @@ def load_json(p: str) -> dict:
     with open(p, 'r') as r:
         args = json.load(r)
     return args
+
 
 def save_embeddings(embeddings: np.ndarray, indexes, path: str) -> None:
     """
