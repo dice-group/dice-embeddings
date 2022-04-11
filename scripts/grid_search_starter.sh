adf3f3dc470f9bb168372f5b63915eff4577e746
Caglar Demir <caglardemir8@gmail.com>
2022-04-11 15:45:07 +0200

    static functions are refactored and more documentations added.

--- scripts/grid_search_starter.sh
+++ scripts/grid_search_starter.sh
@@ -6,19 +6,20 @@ main_wd="$(cd "$PWD"; cd ..; pwd)"
 python_script_path="$main_wd/main.py"
 
 # shellcheck disable=SC2043
-for kgname in "OpenEA_V1.1_EN_FR_15K_V1" "OpenEA_V1.1_EN_FR_15K_V2" "OpenEA_V1.1_EN_FR_100K_V1" "OpenEA_V1.1_EN_FR_100K_V2"
+for kgname in "UMLS"
 do
   kg_path="$main_wd/KGs/$kgname"
-  for model in "Shallom"
+  for model in "DistMult" "ComplEx"
   do
-    for epoch in 1
+    for epoch in 100
     do
-      for dim in 25
+      for dim in 32 64 128 256 512
       do
           # shellcheck disable=SC2154
           config_name="$kgname-$model-$epoch-$dim"
           echo "Running $config_name.log"
-          /bin/bash "$PWD/config_runner.sh" "$python_script_path" "$kg_path" "$model" "$epoch" "$dim" > "$config_name.log"
+          #/bin/bash "$PWD/config_runner.sh" "$python_script_path" "$kg_path" "$model" "$epoch" "$dim" > "$config_name.log"
+          python -u "$python_script_path" --path_dataset_folder "$kg_path" --model "$model" --num_epochs "$epoch" --embedding_dim "$dim" --scoring_technique "KvsAll" > "$config_name.log"
           echo "Done!"
       done
     done
