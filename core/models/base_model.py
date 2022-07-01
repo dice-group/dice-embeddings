535d768f16fb38d2faab5f84b3968b92a388aac3
Caglar Demir <caglardemir8@gmail.com>
2022-07-01 21:53:12 +0200

    momentum added in SGD

--- core/models/base_model.py
+++ core/models/base_model.py
@@ -128,7 +128,7 @@ class BaseKGE(pl.LightningModule):
 
         if self.optimizer_name == 'SGD':
             self.selected_optimizer = torch.optim.SGD(params=self.parameters(), lr=self.learning_rate,
-                                                      momentum=0, dampening=0, weight_decay=self.weight_decay,
+                                                      momentum=0.05, dampening=0, weight_decay=self.weight_decay,
                                                       nesterov=False)
         elif self.optimizer_name == 'Adam':
             self.selected_optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate,
