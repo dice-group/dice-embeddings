bed00726b6ce90cf3f46572ea1ad3334dd3b8056
Caglar Demir <caglardemir8@gmail.com>
2022-07-09 20:28:09 +0200

    Working version of DataParallel and DistrubutedDataParallel started.

--- core/trainers.py
+++ core/trainers.py
@@ -1,4 +1,23 @@
 import torch
+import time
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import os
+os.environ["MASTER_ADDR"] = "localhost"
+os.environ["MASTER_PORT"] = "29500"
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+
 
 
 class CustomTrainer:
@@ -7,6 +26,9 @@ class CustomTrainer:
     def __init__(self, args):
         self.attributes = vars(args)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.loss_function = None
+        self.optimizer = None
+        self.model = None
 
     def __getattr__(self, attr):
         return self.attributes[attr]
@@ -14,40 +36,60 @@ class CustomTrainer:
     def fit(self, *args, **kwargs):
         assert len(args) == 1
         model, = args
-        print(model)
+        self.model = model
         print(kwargs)
         data_loader = kwargs['train_dataloaders']
-        optimizer = model.configure_optimizers()
+        self.loss_function = model.loss_function
+        self.optimizer = model.configure_optimizers()
 
-        if torch.cuda.device_count() > 1:
-            print("Let's use", torch.cuda.device_count(), "GPUs!")
-            # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-            # TODO we also need ModelParallel
-            # We can also store embedding matrixes in CPU
-            # and do the computaiton in GPU
-            # https://pytorch.org/tutorials/beginner/former_torchies/parallelism_tutorial.html
-            # https://fairscale.readthedocs.io/en/latest/tutorials/oss.html ?
-            model = nn.DataParallel(model)
-            self.device = torch.device("cuda:0")
-            model.to(self.device)
-
-        # model = model.to(torch.float)
+        self.model = torch.nn.DataParallel(model)
+        self.model.to(self.device)
+        
+        num_total_batches = len(data_loader)
+        print(f'Number of batches for an epoch:{num_total_batches}')
         for epoch in range(self.attributes['max_epochs']):
             epoch_loss = 0
-            i = 1
+            start_time=time.time()
             for i, z in enumerate(data_loader):
                 # Zero your gradients for every batch!
-                optimizer.zero_grad()
-                # Forward pass
-                batch_loss = model.training_step(batch=[i.to(self.device) for i in z], batch_idx=i)
+                self.optimizer.zero_grad()
+                x_batch, y_batch = z
+                x_batch, y_batch = x_batch.to(self.device), y_batch.to(self.device)
+
+                yhat_batch = self.model(x_batch)#self.compute_forward(z)
+                batch_loss = self.loss_function(yhat_batch, y_batch)
 
                 epoch_loss += batch_loss.item()
+                if i > 0 and num_total_batches % i == 10:
+                    print(f"Batch:{i}\t avg. batch loss until now:\t{epoch_loss / i}")
                 # Backward pass
+                
                 batch_loss.backward()
                 # Adjust learning weights
-                optimizer.step()
-            print(f"{epoch} epoch: Average batch loss:{epoch_loss / i:.3f}")
+                self.optimizer.step()
+            print(f"Epoch took {(time.time()-start_time ) / 60:.3f} minutes")
+            if i>0:
+                print(f"{epoch} epoch: Average batch loss:{epoch_loss / i:.3f}")
+            else:
+                print(f"{epoch} epoch: Average batch loss:{epoch_loss:.3f}")
+
+
+    def compute_forward(self, z):
+        if len(z) == 2:
+            x_batch, y_batch = z
+            x_batch, y_batch = x_batch.to(self.device), y_batch.to(self.device)
+            return self.model.forward(x=x_batch), y_batch
+        elif len(z) == 3:
+            x_batch, y_idx_batch, y_batch, = z
+            x_batch, y_idx_batch, y_batch = x_batch.to(self.device), y_idx_batch.to(self.device), y_batch.to(
+                self.device)
+            return self.model.forward(x=x_batch, y_idx=y_idx_batch), y_batch
+
+        else:
+            print(len(batch))
+            raise ValueError('Unexpected batch shape..')
 
     @staticmethod
     def save_checkpoint(path):
         print('no checkpoint saving')
+
