b6317c4a076c4e02e8abaf5ab53d5a1835e87fdd
Caglar Demir <caglardemir8@gmail.com>
2022-12-27 13:46:52 +0100

    cbd train module is removed as it is not often used.

--- tests/test_continual_training.py
+++ tests/test_continual_training.py
@@ -31,8 +31,7 @@ class TestRegressionCL:
         result = Execute(args).start()
         assert os.path.isdir(result['path_experiment_folder'])
         pre_trained_kge = KGE(path_of_pretrained_model_dir=result['path_experiment_folder'])
-        kg = KG(args.path_dataset_folder, entity_to_idx=pre_trained_kge.entity_to_idx,
-                relation_to_idx=pre_trained_kge.relation_to_idx)
+        kg = KG(data_dir=args.path_dataset_folder)
         pre_trained_kge.train(kg, epoch=1, batch_size=args.batch_size)
 
     @pytest.mark.filterwarnings('ignore::UserWarning')
@@ -60,31 +59,3 @@ class TestRegressionCL:
         kg = KG(args.path_dataset_folder, entity_to_idx=pre_trained_kge.entity_to_idx,
                 relation_to_idx=pre_trained_kge.relation_to_idx)
         pre_trained_kge.train(kg, epoch=1, batch_size=args.batch_size)
-
-    @pytest.mark.filterwarnings('ignore::UserWarning')
-    def test_negative_sampling_Family_cbd_learning(self):
-        args = main_argparse_default([])
-        args.model = 'QMult'
-        args.path_dataset_folder = 'KGs/Family'
-        args.scoring_technique = 'KvsAll'
-        args.optim = 'Adam'
-        args.num_epochs = 1
-        args.batch_size = 1024
-        args.lr = 0.1
-        args.embedding_dim = 32
-        args.input_dropout_rate = 0.0
-        args.hidden_dropout_rate = 0.0
-        args.feature_map_dropout_rate = 0.0
-        args.read_only_few = None
-        args.sample_triples_ratio = None
-        args.num_folds_for_cv = None
-        args.trainer = 'torchCPUTrainer'
-        args.backend = 'pandas'  #  Error with polars becasue sep="\s" should be a single byte character, but is 2 bytes long.
-        result = Execute(args).start()
-        assert os.path.isdir(result['path_experiment_folder'])
-        pre_trained_kge = KGE(path_of_pretrained_model_dir=result['path_experiment_folder'])
-        id_heads = pre_trained_kge.train_set['subject'].value_counts().nlargest(3)
-        entities = {pre_trained_kge.entity_to_idx.iloc[i].name for i in id_heads.to_list()}
-        for ith, entity in enumerate(entities):
-            pre_trained_kge.train_cbd(head_entity=[entity], iteration=1, lr=.01)
-            break
