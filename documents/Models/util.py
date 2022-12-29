5f5dd05954877d34b3082847db06e9b9f136d187
Caglar Demir <caglardemir8@gmail.com>
2022-12-29 15:08:55 +0100

    multiple dim example addded.

--- documents/Models/util.py
+++ documents/Models/util.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import graphviz
 from graphviz import Digraph
+import warnings
+warnings.filterwarnings('ignore')
+
 class Value:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
@@ -59,7 +62,7 @@ class Value:
         out._backward = _backward
         return out
 
-    def backward(self,grad=1):
+    def backward(self, grad=1):
         topo = []
         visited = set()
 
@@ -76,30 +79,33 @@ class Value:
             node._backward()
 
 
-
 def trace(root):
     # builds a set of all nodes and edges in a graph
     nodes, edges = set(), set()
+
     def build(v):
         if v not in nodes:
             nodes.add(v)
             for child in v._prev:
                 edges.add((child, v))
                 build(child)
+
     build(root)
     return nodes, edges
+
+
 def draw_dot(root):
-    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})  # LR = left to right
     nodes, edges = trace(root)
     for n in nodes:
         uid = str(id(n))
         # for any value in the graph, create a rectangular ('record') node for it
-        dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+        dot.node(name=uid, label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
         if n._op:
-          # if this value is a result of some operation, create an op node for it
-          dot.node(name = uid + n._op, label = n._op)
-          # and connect this node to it
-          dot.edge(uid + n._op, uid)
+            # if this value is a result of some operation, create an op node for it
+            dot.node(name=uid + n._op, label=n._op)
+            # and connect this node to it
+            dot.edge(uid + n._op, uid)
 
     for n1, n2 in edges:
         # connect n1 to the op node of n2
