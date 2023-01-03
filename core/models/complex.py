640fe71e10dd81269cf4b4b3b55d02d792c7503c
Caglar Demir <caglardemir8@gmail.com>
2023-01-03 09:50:49 +0100

    AConEx is documented.

--- core/models/complex.py
+++ core/models/complex.py
@@ -164,14 +164,15 @@ class AConEx(BaseKGE):
         # (2) Split (1) into real and imaginary parts.
         emb_head_real, emb_head_imag = torch.hsplit(head_ent_emb, 2)
         emb_rel_real, emb_rel_imag = torch.hsplit(rel_ent_emb, 2)
-
-        # (2) Apply convolution operation on (1).
+        # (3) Apply convolution operation on (1).
         C_3 = self.residual_convolution(C_1=(emb_head_real, emb_head_imag),
                                         C_2=(emb_rel_real, emb_rel_imag))
         a, b, c, d = C_3
+        # (4) Retrieve tail entity embeddings.
         emb_tail_real, emb_tail_imag = torch.hsplit(self.entity_embeddings.weight, 2)
+        # (5) Transpose (4).
         emb_tail_real, emb_tail_imag = emb_tail_real.transpose(1, 0), emb_tail_imag.transpose(1, 0)
-        # (4) Additive connections
+        # (6) Hermitian inner product with additive Conv2D connection.
         real_real_real = torch.mm(a + emb_head_real * emb_rel_real, emb_tail_real)
         real_imag_imag = torch.mm(b + emb_head_real * emb_rel_imag, emb_tail_imag)
         imag_real_imag = torch.mm(c + emb_head_imag * emb_rel_real, emb_tail_imag)
@@ -185,12 +186,11 @@ class AConEx(BaseKGE):
         emb_head_real, emb_head_imag = torch.hsplit(head_ent_emb, 2)
         emb_rel_real, emb_rel_imag = torch.hsplit(rel_ent_emb, 2)
         emb_tail_real, emb_tail_imag = torch.hsplit(tail_ent_emb, 2)
-
         # (2) Apply convolution operation on (1).
         C_3 = self.residual_convolution(C_1=(emb_head_real, emb_head_imag),
                                         C_2=(emb_rel_real, emb_rel_imag))
         a, b, c, d = C_3
-        # (3) Compute hermitian inner product.
+        # (3) Hermitian inner product with additive Conv2D connection.
         real_real_real = (a + emb_head_real * emb_rel_real * emb_tail_real).sum(dim=1)
         real_imag_imag = (b + emb_head_real * emb_rel_imag * emb_tail_imag).sum(dim=1)
         imag_real_imag = (c + emb_head_imag * emb_rel_real * emb_tail_imag).sum(dim=1)
@@ -198,7 +198,6 @@ class AConEx(BaseKGE):
         return real_real_real + real_imag_imag + imag_real_imag - imag_imag_real
 
     def forward_k_vs_sample(self, x: torch.Tensor, target_entity_idx: torch.Tensor):
-        # @OTOD: Double check later.
         # (1) Retrieve embeddings & Apply Dropout & Normalization.
         head_ent_emb, rel_ent_emb = self.get_head_relation_representation(x)
         # (2) Split (1) into real and imaginary parts.
@@ -209,20 +208,25 @@ class AConEx(BaseKGE):
                                         C_2=(emb_rel_real, emb_rel_imag))
         a, b, c, d = C_3
 
-        # (batch size, num. selected entity, dimension)
-        # tail_entity_emb = self.normalize_tail_entity_embeddings(self.entity_embeddings(target_entity_idx))
-        tail_entity_emb = self.entity_embeddings(target_entity_idx)
-        # complex vectors
+        # (4) Retrieve selected tail entity embeddings
+        tail_entity_emb = self.normalize_tail_entity_embeddings(self.entity_embeddings(target_entity_idx))
+        # (5) Split (4) into real and imaginary parts.
         emb_tail_real, emb_tail_i = torch.tensor_split(tail_entity_emb, 2, dim=2)
-
+        # (6) Transpose (5)
         emb_tail_real = emb_tail_real.transpose(1, 2)
         emb_tail_i = emb_tail_i.transpose(1, 2)
-        # Additive Connections
-        real_real_real = torch.bmm((a * emb_head_real * emb_rel_real).unsqueeze(1), emb_tail_real)
-        real_imag_imag = torch.bmm((b * emb_head_real * emb_rel_imag).unsqueeze(1), emb_tail_i)
-        imag_real_imag = torch.bmm((c * emb_head_imag * emb_rel_real).unsqueeze(1), emb_tail_i)
-        imag_imag_real = torch.bmm((d * emb_head_imag * emb_rel_imag).unsqueeze(1), emb_tail_real)
+        # (7) Hermitian inner product with additive Conv2D connection
+        # (7.1) Elementwise multiply (2) according to the Hermitian Inner Product order
+        # (7.2) Additive connection: Add (3) into (7.1)
+        # (7.3) Batch matrix multiplication (7.2) and tail entity embeddings.
+        # https://pytorch.org/docs/stable/generated/torch.bmm.html
+        # input.shape (N, 1, D), mat2.shape (N,D,1)
+        real_real_real = torch.bmm((a + emb_head_real * emb_rel_real).unsqueeze(1), emb_tail_real)
+        real_imag_imag = torch.bmm((b + emb_head_real * emb_rel_imag).unsqueeze(1), emb_tail_i)
+        imag_real_imag = torch.bmm((c + emb_head_imag * emb_rel_real).unsqueeze(1), emb_tail_i)
+        imag_imag_real = torch.bmm((d + emb_head_imag * emb_rel_imag).unsqueeze(1), emb_tail_real)
         score = real_real_real + real_imag_imag + imag_real_imag - imag_imag_real
+        # (N,1,1) => (N,1).
         return score.squeeze(1)
 
 
