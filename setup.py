d4bbdcedee948b87f5d6200e3282c78dfbfca6de
Caglar Demir <caglardemir8@gmail.com>
2023-05-03 11:10:28 +0200

    Setup typos are fixed.

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
                       "polars>=0.16.14",
                       "pyarrow>=11.0.0",
                       'torch>=2.0.0',
-                      "pytorch-lightning==1.6.4"
+                      "pytorch-lightning==1.6.4",
                       "scikit-learn>=1.2.2",
                       "pytest>=7.2.2",
-                      "psutil>=5.9.4"
+                      "psutil>=5.9.4",
                       "gradio>=3.23.0"],
     author='Caglar Demir',
     author_email='caglardemir8@gmail.com',
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License"],
-    python_requires='>==3.10',
+    python_requires='>=3.10',
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
