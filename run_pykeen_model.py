a76954ee2b0b2f2a899f7657dc75547aefb2ca88
Caglar Demir <caglardemir8@gmail.com>
2023-07-11 15:56:37 +0200

    Initial Eval Setting is completed.

--- run_pykeen_model.py
+++ run_pykeen_model.py
@@ -1,6 +1,6 @@
 import pykeen.evaluation.rank_based_evaluator
 from pykeen.datasets import UMLS, Kinships
-from pykeen.models import ERModel, DistMult, Model, ComplEx
+from pykeen.models import ERModel, DistMult, Model, ComplEx, QuatE
 from pykeen.sampling import BasicNegativeSampler
 from torch.optim import Adam
 import time
@@ -8,30 +8,75 @@ import torch
 from pykeen.training import LCWATrainingLoop, SLCWATrainingLoop
 from pykeen.evaluation import LCWAEvaluationLoop, SampledRankBasedEvaluator, RankBasedEvaluator
 
-# @ TODO: Write args to train and eval pykeen models.
-def experiment_umls_distmult():
+from dicee.executer import Execute
+import pytorch_lightning as pl
+from dicee.config import ParseDict
+import argparse
+
+
+def get_default_arguments():
+    """ Extends pytorch_lightning Trainer's arguments with ours """
+    parser = pl.Trainer.add_argparse_args(argparse.ArgumentParser(add_help=False))
+    # Default Trainer param https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#methods
+    parser.add_argument("--dataset", type=str, default='UMLS')
+    parser.add_argument("--model", type=str,
+                        default='ComplEx', help='DistMult, ComplEx')
+    parser.add_argument('--embedding_dim', type=int, default=256,
+                        help='Number of dimensions for an embedding vector. ')
+    parser.add_argument("--num_epochs", type=int, default=100, help='Number of epochs for training. ')
+    parser.add_argument('--batch_size', type=int, default=1024, help='Mini batch size')
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument('--scoring_technique', default='KvsAll', help="KvsAll, NegSample")
+    parser.add_argument('--neg_ratio', type=int, default=1,
+                        help='The number of negative triples generated per positive triple.')
+    return parser.parse_args()
+
+
+def train_eval(args):
     start_time = time.time()
-    dataset = UMLS()
+    if args.dataset == 'UMLS':
+        dataset = UMLS()
+    else:
+        raise NotImplementedError()
+
     print(dataset)
     print('Train:', dataset.training.num_triples)
     print('Val:', dataset.validation.num_triples)
     print('Test:', dataset.testing.num_triples)
     training_triples_factory = dataset.training
-    model = DistMult(triples_factory=training_triples_factory, embedding_dim=128)
-    training_triples_factory = training_triples_factory
-    optimizer = Adam(params=model.get_grad_params())
-    # SLCWA => Negative Sampling
-    training_loop = SLCWATrainingLoop(
-        model=model,
-        triples_factory=training_triples_factory,
-        optimizer=optimizer,
-        optimizer_kwargs=dict(lr=0.01),
-        negative_sampler=BasicNegativeSampler,
-        negative_sampler_kwargs=dict(num_negs_per_pos=1, ), )
-    training_loop.train(triples_factory=training_triples_factory, num_epochs=100, batch_size=1024)
+    if args.model == 'DistMult':
+        model = DistMult(triples_factory=training_triples_factory, embedding_dim=args.embedding_dim)
+    elif args.model == 'ComplEx':
+        model = ComplEx(triples_factory=training_triples_factory, embedding_dim=args.embedding_dim)
+    else:
+        raise NotImplementedError()
 
+    optimizer = Adam(params=model.get_grad_params())
+    if args.scoring_technique == 'KvsAll':
+        # LCWA => KvsAll.
+        training_loop = LCWATrainingLoop(model=model, triples_factory=training_triples_factory,
+                                         optimizer=optimizer, optimizer_kwargs=dict(lr=args.lr))
+    elif args.scoring_technique == 'NegSample':
+        assert args.neg_ratio > 0
+        # SLCWA => Negative Sampling
+        training_loop = SLCWATrainingLoop(
+            model=model,
+            triples_factory=training_triples_factory,
+            optimizer=optimizer,
+            optimizer_kwargs=dict(lr=args.lr),
+            negative_sampler=BasicNegativeSampler,
+            negative_sampler_kwargs=dict(num_negs_per_pos=args.neg_ratio, ))
+    else:
+        raise KeyError('Invalid Argument')
+
+    training_loop.train(triples_factory=training_triples_factory, num_epochs=args.num_epochs,
+                        batch_size=args.batch_size)
     report_time = time.time() - start_time
-    # Pick an evaluation loop (NEW)
+    print('Total Training Time:', report_time)
+    test(model, dataset, args)
+
+
+def test(model, dataset, args):
     evaluator = RankBasedEvaluator()
     # Get triples to test
     mapped_triples = dataset.testing.mapped_triples
@@ -39,218 +84,13 @@ def experiment_umls_distmult():
     results = evaluator.evaluate(
         model=model,
         mapped_triples=mapped_triples,
-        batch_size=512,
+        batch_size=args.batch_size,
         additional_filter_triples=[dataset.training.mapped_triples, dataset.validation.mapped_triples], )
-    return report_time, results
 
-
-def experiment_umls_complex():
-    start_time = time.time()
-
-    dataset = UMLS()
-    training_triples_factory = dataset.training
-    model = ComplEx(triples_factory=training_triples_factory,
-                    embedding_dim=1,
-
-                    )
-    model.to(torch.device('cuda'))
-    # model.to(torch.device('cpu'))
-    training_triples_factory = training_triples_factory
-    optimizer = Adam(params=model.get_grad_params())
-
-    training_loop = SLCWATrainingLoop(
-        model=model,
-        triples_factory=training_triples_factory,
-        optimizer=optimizer,
-        optimizer_kwargs=dict(lr=0.1),
-        negative_sampler=BasicNegativeSampler,
-        negative_sampler_kwargs=dict(
-            num_negs_per_pos=1,
-        ),
-    )
-
-    # training_loop = LCWATrainingLoop(
-    #   model=model,
-    #   triples_factory=training_triples_factory,
-    #   optimizer=optimizer,
-    #   optimizer_kwargs=dict(lr=0.1),
-
-    # )
-
-    _ = training_loop.train(
-        triples_factory=training_triples_factory,
-        num_epochs=100,
-        batch_size=512,
-
-    )
-
-    report_time = time.time() - start_time
-
-    evaluator = RankBasedEvaluator()
-
-    # Get triples to test
-    mapped_triples = dataset.testing.mapped_triples
-
-    # Evaluate
-    results = evaluator.evaluate(
-        model=model,
-        mapped_triples=mapped_triples,
-        batch_size=512,
-        additional_filter_triples=[
-            dataset.training.mapped_triples,
-            dataset.validation.mapped_triples,
-        ],
-    )
-
-    return report_time, results
-
-
-def experiment_kinship_distmult():
-    start_time = time.time()
-
-    dataset = Kinships()
-    training_triples_factory = dataset.training
-    model = DistMult(triples_factory=training_triples_factory,
-                     embedding_dim=64,
-
-                     )
-    model.to(torch.device('cuda'))
-    # model.to(torch.device('cpu'))
-    training_triples_factory = training_triples_factory
-    optimizer = Adam(params=model.get_grad_params())
-
-    training_loop = SLCWATrainingLoop(
-        model=model,
-        triples_factory=training_triples_factory,
-        optimizer=optimizer,
-        optimizer_kwargs=dict(lr=0.1),
-        negative_sampler=BasicNegativeSampler,
-        negative_sampler_kwargs=dict(
-            num_negs_per_pos=1,
-        ),
-    )
-
-    # training_loop = LCWATrainingLoop(
-    #   model=model,
-    #   triples_factory=training_triples_factory,
-    #   optimizer=optimizer,
-    #   optimizer_kwargs=dict(lr=0.1),
-
-    # )
-
-    _ = training_loop.train(
-        triples_factory=training_triples_factory,
-        num_epochs=100,
-        batch_size=512,
-
-    )
-
-    report_time = time.time() - start_time
-
-    evaluator = RankBasedEvaluator()
-
-    # Get triples to test
-    mapped_triples = dataset.testing.mapped_triples
-
-    # Evaluate
-    results = evaluator.evaluate(
-        model=model,
-        mapped_triples=mapped_triples,
-        batch_size=512,
-        additional_filter_triples=[
-            dataset.training.mapped_triples,
-            dataset.validation.mapped_triples,
-        ],
-    )
-
-    return report_time, results
-
-
-def experiment_kinship_complex():
-    start_time = time.time()
-
-    dataset = Kinships()
-    training_triples_factory = dataset.training
-    model = ComplEx(triples_factory=training_triples_factory,
-                    embedding_dim=32,
-
-                    )
-
-    model.to(torch.device('cuda'))
-    # model.to(torch.device('cpu'))
-    training_triples_factory = training_triples_factory
-    optimizer = Adam(params=model.get_grad_params())
-
-    training_loop = SLCWATrainingLoop(
-        model=model,
-        triples_factory=training_triples_factory,
-        optimizer=optimizer,
-        optimizer_kwargs=dict(lr=0.1),
-        negative_sampler=BasicNegativeSampler,
-        negative_sampler_kwargs=dict(
-            num_negs_per_pos=1,
-        ),
-    )
-
-    # training_loop = LCWATrainingLoop(
-    #   model=model,
-    #   triples_factory=training_triples_factory,
-    #   optimizer=optimizer,
-    #   optimizer_kwargs=dict(lr=0.1),
-
-    # )
-
-    _ = training_loop.train(
-        triples_factory=training_triples_factory,
-        num_epochs=100,
-        batch_size=512,
-
-    )
-
-    report_time = time.time() - start_time
-
-    evaluator = RankBasedEvaluator()
-
-    # Get triples to test
-    mapped_triples = dataset.testing.mapped_triples
-
-    # Evaluate
-    results = evaluator.evaluate(
-        model=model,
-        mapped_triples=mapped_triples,
-        batch_size=512,
-        additional_filter_triples=[
-            dataset.training.mapped_triples,
-            dataset.validation.mapped_triples,
-        ],
-    )
-
-    return report_time, results
-
-
-def save_evaluation(results, path):
-    # Pickle the object and save it to a file
-    file_path = path
-
-    # Save the dictionary as text in a file
-    with open(file_path, 'w') as file:
-        for key, value in results.data.items():
-            file.write(f"{key}: {value}\n")
-
-
-def save_report(runtime, filename):
-    res = dict()
-    res['report_time'] = runtime
-
-    with open(filename + '.json', 'w') as fp:
-        import json
-        json.dump(res, fp)
-        print('dictionary saved successfully to file')
-
-
-runtime, results = experiment_umls_distmult()
-# @OTOD:Report Link Prediction Results..
-for key,val in results.data.items():
-    if key[0] in ['hits_at_1','hits_at_3','hits_at_10']:
-        hit_name, head_or_tail, optimistic_pessimistic_realistic=key
-        print(key,val)
+    for key, val in results.data.items():
+        if key[0] in ['hits_at_1', 'hits_at_3', 'hits_at_10']:
+            hit_name, head_or_tail_or_both, optimistic_pessimistic_realistic = key
+            if optimistic_pessimistic_realistic == 'pessimistic' and head_or_tail_or_both == 'both':
+                print(key, val)
+if __name__ == '__main__':
+    pretrained_model = train_eval(get_default_arguments())
