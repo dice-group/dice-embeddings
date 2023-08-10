d2e86c41b2092aace66b56d95e8304cb3e02f161
Caglar Demir <caglardemir8@gmail.com>
2023-08-10 15:54:00 +0200

    Increased the new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='dicee',
     description='Dice embedding is an hardware-agnostic framework for large-scale knowledge graph embedding applications',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0",
