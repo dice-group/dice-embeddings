fd9fbf97f7bd29b4240494132af8e7d716e4fa22
Caglar Demir <caglardemir8@gmail.com>
2023-08-12 13:24:20 +0200

    Resume training fix

--- large_scale_training.py
+++ large_scale_training.py
@@ -23,14 +23,14 @@ def input_arguments():
                         help="path parquet formatted polars dataframe")
     parser.add_argument("--path_idx_kg", type=str, default="data.npy",
                         help="path to numpy ndarray")
-    parser.add_argument("--path_checkpoint", type=str, default="Keci_1_99.torch"
+    parser.add_argument("--path_checkpoint", type=str, default="Keci_1_9.torch"
                         )
-    parser.add_argument("--path_checkpoint2", type=str, default="Keci_2_90.torch")
+    parser.add_argument("--path_checkpoint2", type=str, default="Keci_2_9.torch")
 
-    parser.add_argument("--batch_size", type=int, default=1024)
+    parser.add_argument("--batch_size", type=int, default=10_000_000)
     parser.add_argument("--neg_sample_ratio", type=float, default=1.0)
-    parser.add_argument("--embedding_dim", type=int, default=100)
-    parser.add_argument("--num_epochs", type=int, default=100)
+    parser.add_argument("--embedding_dim", type=int, default=20)
+    parser.add_argument("--num_epochs", type=int, default=40)
 
     return parser.parse_args()
 
@@ -85,9 +85,9 @@ def get_data(args) -> Tuple[np.ndarray, int, int]:
 
     elif args.path_idx_kg:
         print("Loading the index numpy KG..\n")
+        #data=np.load('data.npy', mmap_mode='r')
         with open("data.npy", 'rb') as f:
             data = np.load(f)
-
         num_entities = 1 + max(max(data[:, 0]), max(data[:, 2]))
         num_relations = 1 + max(data[:, 1])
         return data, num_entities, num_relations
@@ -116,13 +116,13 @@ def get_model(args, num_entities: int, num_relations: int):
         model1, model2 = models
         opt1, opt2 = optimizers
 
-        checkpoint1 = torch.load(args.path_checkpoint)
+        checkpoint1 = torch.load(args.path_checkpoint,map_location='cpu')
         model1.load_state_dict(checkpoint1['model_state_dict'])
-        opt1.load_state_dict(checkpoint1['optimizer_state_dict'])
+        #opt1.load_state_dict(checkpoint1['optimizer_state_dict'])
 
-        checkpoint2 = torch.load(args.path_checkpoint2)
+        checkpoint2 = torch.load(args.path_checkpoint2,map_location='cpu')
         model2.load_state_dict(checkpoint2['model_state_dict'])
-        opt2.load_state_dict(checkpoint2['optimizer_state_dict'])
+        #opt2.load_state_dict(checkpoint2['optimizer_state_dict'])
         models = (model1, model2)
         optimizers = (opt1, opt2)
     return models, optimizers
@@ -132,14 +132,14 @@ def get_train_loader(args):
     data: np.ndarray
     data, num_ent, num_rel = get_data(args)
     data: torch.utils.data.DataLoader
-
+    print('Creating dataset...')
     data: NegSampleDataset
     data = NegSampleDataset(train_set=data,
                             num_entities=num_ent, num_relations=num_rel,
                             neg_sample_ratio=1.0)
     data: torch.utils.data.DataLoader
     data = torch.utils.data.DataLoader(dataset=data, batch_size=args.batch_size, shuffle=True,
-                                       num_workers=os.cpu_count() - 1)
+                                       num_workers=10)
     return data, num_ent, num_rel
 
 
@@ -149,20 +149,32 @@ def run(args):
     dataloader, num_ent, num_rel = get_train_loader(args)
     num_triples = len(dataloader.dataset)
     print('Number of triples', num_triples)
+    # (2) Get model
     models, optimizers = get_model(args, num_ent, num_rel)
 
+    # (3) Compile models
     model1, model2 = models
-
+    print('####### Model 1 #######')
+    print(model1)
+    print(model1.summarize())
     model1 = torch.compile(model1)
+    print(model1)
+    print('######## Model2 #######')
+    print(model2)
+    print(model2.summarize())
     model2 = torch.compile(model2)
+    print(model2)
+    # (4) Get optim
     opt1, opt2 = optimizers
-
-    # Define the loss function
+    print(opt1)
+    print(opt2)
+    # (5) Get loss func
     loss_function = model1.loss_function
     print("Training...")
 
-    device1 = "cpu"  # "cuda:0"
-    device2 = "cpu"  # "cuda:1"
+    device1 = "cuda:0"
+    device2 = "cuda:1"
+
     for e in range(args.num_epochs):
         epoch_loss = 0
 
@@ -170,9 +182,11 @@ def run(args):
             # (1) Shape the batch
             x = x.flatten(start_dim=0, end_dim=1)
             y = y.flatten(start_dim=0, end_dim=1)
+            
             # (2) Empty the gradients
             opt1.zero_grad(set_to_none=True)
             opt2.zero_grad(set_to_none=True)
+            
             # (3) Forward Backward and Parameter Update
             start_time = time.time()
             # (3.1) Select embeddings of triples
@@ -195,22 +209,21 @@ def run(args):
             # 3.6 Forward Pass
             yhat2 = model2.score(h2, r2, t2).to(device1)
 
-            # (4) Composite Prediction
+            # (3.7) Composite Prediction
             yhat = yhat1 + yhat2
-
-            # (5.) Compute Loss
+            # (3.8) Compute Loss
             batch_loss = loss_function(yhat, y)
-            # (6.) Compute gradients (Backward Pass)
+            # (3.9) Compute gradients (Backward Pass)
             batch_loss.backward()
-            # (3.6) Update parameters
+            # (3.10) Update parameters
             opt1.step()
             opt2.step()
-            # (3.7) Update epoch loss
+            # (4) Update epoch loss
             batch_loss = batch_loss.item()
             epoch_loss += batch_loss
-            if ith % 5 == 0:
+            if ith % 1 == 0: # init an argument
                 print(f"Batch Loss:{batch_loss}\tForward-Backward-Update: {time.time() - start_time}")
-
+            
         print(f"Epoch loss:{epoch_loss}")
         print('Saving..')
         torch.save({
