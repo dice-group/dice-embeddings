058163e4ee4100641658d628478a8991e2a37a33
louis888 <louismozart.teyou@aims.ac.rw>
2023-10-03 14:19:20 +0200

    -WIP: A naive working version for  parameter searching

--- dicee/loop_main.py
+++ dicee/loop_main.py
@@ -2,31 +2,46 @@ import subprocess
 import sys
 from static_funcs_training import evaluate_lp
 import json
+import pandas as pd
 
-parameter_values = [
-    {"p": 1, "q": 0, "r": 0},
-    {"p": 2, "q": 0, "r": 0},
-    {"p": 3, "q": 0, "r": 0},
-]
+parameter_values = [0,1]
+
+results = dict()
 
 evaluate_lp_outputs = []
-for i in range(5):
-    subprocess_output = subprocess.run(["python", "/home/dice/Desktop/dice-embeddings/main.py", "--num_epochs",str(i)],stdout=subprocess.PIPE,universal_newlines=True,)
-    #eval_output = subprocess_output.stdout.strip()
-    #print(evaluate_lp(eval_output))
+df = pd.DataFrame()
 
-    try:
-        output_dict = json.loads(subprocess_output.stdout)
-        print(output_dict)
-        evaluate_lp_result = evaluate_lp(output_dict)  # Call the imported evaluate_lp function
+for p in parameter_values:
+    for q in parameter_values:
+        for r in parameter_values:
+           
+           if 16%(p+q+r+1) == 0:
+                subprocess_output = subprocess.run(["python", "/home/dice/Desktop/dice-embeddings/main.py" ,"--p",str(p)\
+                                ,"--q",str(q), "--r", str(r), "--neg_ratio",str(50),"--batch_size",str(32),"--num_epochs",str(100)],stdout=subprocess.PIPE,universal_newlines=True,)
+                eval_output = subprocess_output.stdout.strip()
+
+                eval_output = eval_output[-147:-30]
+
+                #print(eval_output)
+
+                start_index = eval_output.find("{")
+
+                data_str = eval_output[start_index:]
+
+                data_str = data_str.replace("'", "\"")
+
+                data_dict = json.loads(data_str)
+
+                results.update({'p':p,'q':q,'r':r})
+                results.update(data_dict)
+                
+                dfi = pd.DataFrame(results, index = [0])
+
+                df = pd.concat([df,dfi])
+
+print(df)          
 
 
-        evaluate_lp_outputs.append(evaluate_lp_result)
-    except json.JSONDecodeError:
-        # Handle JSON decoding errors if necessary
-        print(f"Subprocess output for iteration {i}:")
-        print(subprocess_output.stdout)
-
-print(evaluate_lp_outputs)
+#print(evaluate_lp_outputs)
     
     
