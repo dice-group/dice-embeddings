0ba9475870b6f244c1e66c703a72e7c62b131cef
louis888 <louismozart.teyou@aims.ac.rw>
2023-10-11 12:41:09 +0200

    Work done: Keci_r evaluation codes

--- dicee/keci_r_MRR.py
+++ dicee/keci_r_MRR.py
@@ -1,21 +1,195 @@
 import subprocess
 import json
+import pandas as pd
 
-def MRR(p,q,r):
 
-    subprocess_output = subprocess.run(["python", "/home/dice/Desktop/dice-embeddings/main.py" ,"--p",str(p)\
-                            ,"--q",str(q), "--r", str(r), "--neg_ratio",str(50),"--batch_size",str(32),"--num_epochs",str(100)],stdout=subprocess.PIPE,universal_newlines=True,)
-    eval_output = subprocess_output.stdout.strip()
-    
-    result_output = eval_output[-147:-30]
 
-    start_index = result_output.find("{")
+class Keci_exp:
 
-    data_str = result_output[start_index:]
+    def __init__(self, emb_dim : int , path_main : str, num_epochs : int ,batch_size : int, neg_ratio : int):
 
-    data_str = data_str.replace("'", "\"")
+        self.parameter_values = range(emb_dim+1)
+        self.path_main = path_main
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.neg_ratio = neg_ratio
+        self.emb_dim = emb_dim
+        self.results = dict()
+        
 
-    data_dict = json.loads(data_str)
+    def results_keci(self) -> pd.DataFrame :
+        '''This function store the performance of the Keci_r model into a dataframe for all possible values of p,q,r in [0, emb_dim]'''
 
-    return data_dict['MRR']
+        df = pd.DataFrame()
+
+        i = 0 #for indexing
+        for p in self.parameter_values:
+            for q in self.parameter_values:
+                for r in self.parameter_values:
+                
+                    if 16%(p+q+r+1) == 0:
+                            subprocess_output = subprocess.run(["python", self.path_main,"--p",str(p) ,"--q",str(q), "--r", str(r), "--neg_ratio",str(self.neg_ratio)\
+                                            ,"--batch_size",str(self.batch_size),"--num_epochs",str(self.num_epochs)],stdout=subprocess.PIPE,universal_newlines=True,)
+                            eval_output = subprocess_output.stdout.strip()
+
+                            result_output = eval_output[-147:-30]
+                            #print(result_output)
+
+                            time_output = eval_output[-29:-1]
+                            #print(time_output)
+                            parts = time_output.split()
+                            run_time = float(parts[-2])
+
+                            start_index = result_output.find("{")
+
+                            data_str = result_output[start_index:]
+
+                            data_str = data_str.replace("'", "\"")
+
+                            data_dict = json.loads(data_str)
+
+                            self.results.update({'p':p,'q':q,'r':r})
+                            self.results.update(data_dict)
+                            self.results.update({'time(s)': run_time})
+                            
+                            dfi = pd.DataFrame(self.results, index = [i])
+
+                            df = pd.concat([df,dfi])
+
+                            i += 1
+        # df.to_csv('keci_exp_UMLS.csv') #save as csv.
+        return df
+
+
+
+    def params_search(self, start_point = (1,1,1)):
+         
+        '''Gradient based searching parameters starting at a fixed point named start point.
+        Default starting point is (1,1,1)
+        NB: here the best performance is achieved with the highest MRR
+        Return a tuple with the best triple (p,q,r), the corresponding MRR and the list of all the other configurations.'''
+
+        (opt_p, opt_q, opt_r) = start_point
+        p0, q0, r0 = opt_p, opt_q, opt_r
+        max_MRR = 0
+        l = []
+        d = self.emb_dim
+
+        for p in range(p0, d):
+            for q in range(q0, d):
+                for r in range(q0, d):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+        for p in range(p0, -1, -1):
+            for q in range(q0, -1, -1):
+                for r in range(q0, -1, -1):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+        for p in range(p0, d):
+            for q in range(q0, -1, -1):
+                for r in range(r0, -1, -1):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+        for p in range(p0, -1, -1):
+            for q in range(q0, d):
+                for r in range(r0, -1, -1):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+
+        for p in range(p0, -1, -1):
+            for q in range(q0, -1, -1):
+                for r in range(r0, d):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+
+        for p in range(p0, d):
+            for q in range(q0, d):
+                for r in range(r0, -1, -1):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+
+        for p in range(p0, d):
+            for q in range(q0, -1, -1):
+                for r in range(r0, d):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+
+        for p in range(p0, -1, -1):
+            for q in range(q0, d):
+                for r in range(r0, d):
+                    if ((p,q,r) not in l) & (16%(1+p+q+r)==0):
+                        l.append((p, q, r))
+                        Mrr = self.MRR(p,q,r)
+
+                        if Mrr > max_MRR:
+                            max_MRR = Mrr
+                            (opt_p, opt_q, opt_r) = (p,q,r)
+
+        # print(l)
+        # print(max_MRR)
+        # print((opt_p,opt_q,opt_r))
+
+        return (opt_p,opt_q,opt_r), max_MRR, l
+
+    def MRR(self, p,q,r):
+        '''Return the achieved MRR of Keci_r for a fixed p,q and r'''
+
+        subprocess_output = subprocess.run(["python", self.path_main,"--p",str(p) ,"--q",str(q), "--r", str(r), "--neg_ratio",str(self.neg_ratio)\
+                                            ,"--batch_size",str(self.batch_size),"--num_epochs",str(self.num_epochs)],stdout=subprocess.PIPE,universal_newlines=True,)
+        eval_output = subprocess_output.stdout.strip()
+        
+        result_output = eval_output[-147:-30]
+
+        start_index = result_output.find("{")
+
+        data_str = result_output[start_index:]
+
+        data_str = data_str.replace("'", "\"")
+
+        data_dict = json.loads(data_str)
+
+        return data_dict['MRR']
+
 
