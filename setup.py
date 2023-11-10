c5145ea2fd43eca17edd14a1cfd195274291c649
Caglar Demir <caglardemir8@gmail.com>
2023-11-10 13:07:24 +0100

    Fix typo in the version and beautifulsoup dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
 setup(
     name="dicee",
     description="Dice embedding is an hardware-agnostic framework for large-scale knowledge graph embedding applications",
-    version="0.0.9",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "torch>=2.0.0",
