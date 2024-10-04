1083598fc3fd5119b081151bb162f318b5cbbd33
Caglar Demir <caglardemir8@gmail.com>
2024-10-04 12:00:32 +0200

    Refactoring In Progress: unused imports and docstrings

--- dicee/models/transformers.py
+++ dicee/models/transformers.py
@@ -1,6 +1,3 @@
-from .base_model import BaseKGE
-import torch
-
 """
 Full definition of a GPT Language Model, all of it in this single file.
 References:
@@ -9,11 +6,10 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 2) huggingface/transformers PyTorch implementation:
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
-
+from .base_model import BaseKGE
 import math
 import inspect
 from dataclasses import dataclass
-
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
