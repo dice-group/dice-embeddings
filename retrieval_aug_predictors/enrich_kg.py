674e9f5e521e304691ef063f9f79b23e0a5f8ef2
alkidbaci <alkid1baci@gmail.com>
2025-04-28 14:15:34 +0200

    added --model arg

--- retrieval_aug_predictors/enrich_kg.py
+++ retrieval_aug_predictors/enrich_kg.py
@@ -15,7 +15,7 @@ def run(args):
 
 
     if not os.path.exists(args.pred_out):
-        subprocess.run(["python", "models/RALP.py",
+        subprocess.run(["python", "models/"+ args.model +".py",
                         "--dataset_dir", args.dataset_dir,
                         "--print_top_predictions",
                         "--out", args.pred_out])
@@ -44,18 +44,20 @@ def run(args):
 
 
 if __name__ == "__main__":
-    """If run on default arguments (with exception of `dataset_dir` which has to be set), it will create prediction 
-    using RALP, enrich the train set of the KG with these predictions and save it to a directory named 
-    "Enriched_<dataset_name>". Test and validation splits are copied as they are from the original dataset.
+    """Creates predictions using the specified 'model' using train set of the specified dataset,
+    enriches the train set with the predictions and save it to a directory named 
+    'Enriched_<dataset_name>' if arg `kg_out` is not specified. 
+    Test and validation splits are copied as they are from the original dataset.
     """
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--dataset_dir", type=str,
                         default="/home/alkid/PycharmProjects/dice-embeddings/KGs/Countries-S3",
                         help="Path to the dataset.")
+    parser.add_argument("--model", type=str, default="RALP", choices=["RALP", "RALP_mipro"],
+                        help="Name of the output file where the predictions will be saved.")
     parser.add_argument("--pred_out", type=str, default=None,
                         help="Name of the output file where the predictions will be saved.")
-
     parser.add_argument("--kg_out", type=str, default=None,
                         help="Name of the output file where the extended train set will be saved.")
 
