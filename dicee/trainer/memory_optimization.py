a350225c75ed9caf359fa86efca86373b3f03d59
Pritilata Saha <pritilata19saha95@gmail.com>
2025-09-09 06:57:29 +0200

    Trainer for optimized GPU use

--- dicee/trainer/memory_optimization.py
+++ dicee/trainer/memory_optimization.py
@@ -28,7 +28,7 @@ class MemoryOptimizedTrainer(AbstractTrainer):
         # CPU cache settings
         self.threshold = self.args.threshold
         self.entity_capacity = getattr(self.args, "max_entities", self.threshold)
-        self.entity_embed_cache = {}
+        self.entity_embed_cache = {}    # {ent_id: embedding}
         self.entity_id_to_slot = {}
         self.slot_to_entity_id = {}
         self.next_free_slot = 0
@@ -37,6 +37,7 @@ class MemoryOptimizedTrainer(AbstractTrainer):
               f'# of CPUs for dataloader:{self.args.num_core}')
         for i in range(torch.cuda.device_count()):
             print(torch.cuda.get_device_name(i))
+
 
     # Training
     def fit(self, *args, train_dataloaders, **kwargs) -> None:
@@ -65,13 +66,16 @@ class MemoryOptimizedTrainer(AbstractTrainer):
 
     def _wrap_training_step(self):
         orig_train_step = self.model.training_step
-
         def wrapped_training_step(batch, batch_idx):
-            entity_ids = torch.unique(torch.cat([batch[0][0], batch[0][1]])).tolist()
+            s_ids = torch.stack([item[0] for item in batch[0]])
+            o_ids = torch.stack([item[1] for item in batch[0]])
+
+            entity_ids = torch.unique(torch.cat([s_ids, o_ids])).tolist()
+
+            #print("no. of unique entity_ids in batch: ", len(entity_ids))
 
             #  Load embeddings from CPU cache into GPU slots
             self._hydrate_model_from_cache(entity_ids)
-
 
             loss = orig_train_step(batch, batch_idx)
 
@@ -89,9 +93,12 @@ class MemoryOptimizedTrainer(AbstractTrainer):
         for eid in entity_ids:
             slot = self.entity_id_to_slot.get(eid, None)
             if slot is None:
+                # assign a new slot
                 if self.next_free_slot >= self.entity_capacity:
                     evict_slot = np.random.choice(list(self.slot_to_entity_id.keys()))
                     evict_eid = self.slot_to_entity_id[evict_slot]
+                    #print("entity capacity full, evict_eid, slot: ", evict_eid, evict_slot)
+                    # save evicted entity to CPU cache
                     self.entity_embed_cache[evict_eid] = self.model.entity_embeddings.weight[evict_slot].detach().cpu().clone()
                     del self.entity_id_to_slot[evict_eid]
                     del self.slot_to_entity_id[evict_slot]
@@ -103,6 +110,7 @@ class MemoryOptimizedTrainer(AbstractTrainer):
                 self.entity_id_to_slot[eid] = slot
                 self.slot_to_entity_id[slot] = eid
 
+                # initialize embedding from cache if exists, else random init
                 if eid in self.entity_embed_cache:
                     self.model.entity_embeddings.weight[slot].data.copy_(
                         self.entity_embed_cache[eid].to(self.model.entity_embeddings.weight.device)
@@ -116,3 +124,4 @@ class MemoryOptimizedTrainer(AbstractTrainer):
             slot = self.entity_id_to_slot.get(eid, None)
             if slot is not None:
                 self.entity_embed_cache[eid] = self.model.entity_embeddings.weight[slot].detach().cpu().clone()
+                #print("embedding saved to cpu entity_embed_cache")
