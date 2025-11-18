fc358c62bb192e1483655c8cb8523bb89054b4c8
Luke Friedrichs <lukefriedrichs@gmail.com>
2025-11-18 12:28:03 +0100

    implementation of CoKE_kge model; also changed CausalSelfAttention  in transfromers.py to SelfAttention which supports a bool causal paramter since CoKE needs non-causal attention

--- dicee/models/real.py
+++ dicee/models/real.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from .base_model import BaseKGE
 from typing import Tuple
 import torch
@@ -127,3 +128,63 @@ class Pyke(BaseKGE):
         dist_rel_tail = self.dist_func(rel_ent_emb, tail_ent_emb)
         avg_dist = (dist_head_rel + dist_rel_tail) / 2
         return self.margin - avg_dist
+
+@dataclass
+class CoKEConfig:
+    block_size: int = 3           # triples -> TODO: LF: for multi-hop this needs to be bigger
+    vocab_size: int = None        # Must be set to num_entities + num_relations before initializing CoKE
+    n_layer: int = 12             
+    n_head: int = 4               
+    n_embd: int = None             
+    dropout: float = 0.1          # according to paper in [0.1 - 0.5]
+    bias: bool = True             # idk if better with false?
+    causal: bool = False          # non-causal so that we gather information in mask token 
+
+from dicee.models.transformers import Block
+from torch import nn
+
+class CoKE(BaseKGE):
+    def __init__(self, args, config: CoKEConfig):
+        super().__init__(args)
+        self.name = 'CoKE'
+
+        self.config = config
+        self.config.vocab_size = self.num_entities + self.num_relations
+        self.config.n_embd = self.embedding_dim
+    
+        self.entity_relation_embeddings = torch.nn.Embedding(config.vocab_size, self.embedding_dim)
+        self.pos_emb = torch.nn.Embedding(config.block_size, self.embedding_dim)
+        self.mask_emb = torch.nn.Parameter(torch.zeros(self.embedding_dim))
+
+        # use transformer Block from transformers.py
+        self.blocks = torch.nn.ModuleList([Block(config) for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(self.embedding_dim)
+
+        self.coke_dropout = nn.Dropout(config.dropout)
+
+    #currently only tail prediction
+    def forward_k_vs_all(self, x: torch.Tensor):
+        device = x.device
+        b = x.size(dim=0)
+
+        head_emb, rel_emb = self.get_head_relation_representation(x) # (b, dim), (b, dim)
+        mask_emb = self.mask_emb.unsqueeze(0).expand(b, -1) # (b, dim)
+        seq = torch.stack([head_emb, rel_emb, mask_emb], dim=1) # (b, 3, dim)
+        pos_ids = torch.arange(0, 3, device=device) # (3,) -> TODO: LF: here 3 has to change according to voacb size (in case we want multi-hop)
+        pos_ids = pos_ids.unsqueeze(0).expand(b, 3) #  TODO: LF: same as above
+        pos_emb = self.pos_emb(pos_ids)  # (b, 3, dim)
+        x_tok = seq + pos_emb # (b, 3, dim)
+
+        for block in self.blocks:
+            x_tok = block(x_tok) # transformer foward pass  
+        x_tok = self.ln_f(x_tok) #final layernorm
+
+        h_mask = x_tok[:, 2, :] #hidden state of mask 
+        h_mask = self.coke_dropout(h_mask) 
+
+        E = self.entity_embeddings.weight
+        E = self.normalize_tail_entity_embeddings(E) 
+
+        scores = h_mask.mm(E.t()) 
+
+        return scores 
