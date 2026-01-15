86b4aca24f53f73fa17ba2d5f973cc4cf78aff4e
Luke Friedrichs <lukefriedrichs@gmail.com>
2026-01-15 12:21:00 +0100

    seperate script for plotting results

--- dicee/bytegen/run.py
+++ dicee/bytegen/run.py
@@ -14,25 +14,26 @@ if __name__ == "__main__":
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     wandb.init(project="bytegen-experiment")
     # Setup
-    dataset_path = os.path.join(os.getcwd(), "KGs/Countries-S3")
+    dataset_path = os.path.join(os.getcwd(), "KGs/UMLS")
     
     # Initialize Tokenizer (with inverse=True to match dataset)
     USE_INVERSE = True
-    tokenizer_path = "tokenizer.json"
-    tokenizer = train_bpe_tokenizer(dataset_path, tokenizer_path, vocab_size=1024, inverse=USE_INVERSE)
-    
+    # tokenizer_path = "tokenizer.json"
+    # tokenizer = train_bpe_tokenizer(dataset_path, tokenizer_path, vocab_size=1024, inverse=USE_INVERSE)
+    tokenizer = ByteTokenizer()
+
     BFS_BLOCK_SIZE = 256
     
-    train_ds = ByteGenBFSDataset(dataset_path, tokenizer, split='train', block_size=BFS_BLOCK_SIZE, inverse=USE_INVERSE)
-    test_ds = ByteGenBFSDataset(dataset_path, tokenizer, split='test', block_size=BFS_BLOCK_SIZE)
+    train_ds = ByteGenDataset(dataset_path, tokenizer, split='train', block_size=BFS_BLOCK_SIZE, inverse=USE_INVERSE)
+    test_ds = ByteGenDataset(dataset_path, tokenizer, split='test', block_size=BFS_BLOCK_SIZE, inverse=USE_INVERSE)
     
     conf = ByteGenConfig(
         block_size=BFS_BLOCK_SIZE,  # Larger for BFS sequences
-        n_layer=8, 
-        n_head=8, 
-        n_embd=512, 
-        dropout=0.1,  # Add dropout for generalization
-        batch_size=64,
+        n_layer=6, 
+        n_head=2, 
+        n_embd=16, 
+        dropout=0.0,  # Add dropout for generalization
+        batch_size=128,
         lr=3e-4,
         vocab_size=tokenizer.vocab_size
     )
@@ -46,7 +47,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.AdamW(model.parameters(), lr=conf.lr)
     
     # Trainer - with H@1 diagnostic every 10 epochs
-    EPOCHS = 300
+    EPOCHS = 100
     trainer = Trainer(model, train_loader, conf, tokenizer, optimizer, 
                       label_smoothing=0.1, warmup_epochs=5,
                       train_dataset=train_ds)
