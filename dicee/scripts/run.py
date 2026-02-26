c4ff4237619210df0f487c9bcaedc7e42d27051f
sapkotaruz11 <sapkotarupesh15@gmail.com>
2026-02-26 11:05:24 +0100

    undo changes run.py

--- dicee/scripts/run.py
+++ dicee/scripts/run.py
@@ -27,7 +27,7 @@ def get_default_arguments(description=None):
     parser.add_argument("--backend", type=str, default="pandas",
                         choices=["pandas", "polars", "rdflib"],
                         help='Backend for loading, preprocessing, indexing input knowledge graph.')
-    parser.add_argument("--separator", type=str, default="\\s+",
+    parser.add_argument("--separator", type=str, default="\s+",
                         help='Pandas \s+, t for \t polars works with the last two.')
     parser.add_argument("--reuse_existing_run_dir", action="store_true",
                         help="If set, reuse the existing path_to_store_single_run directory if it exists. "
