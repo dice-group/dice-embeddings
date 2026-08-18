e3af2b801f0362c671a15b3ce2914ffbbcada300
Shivam Sharma <sshivam95@gmail.com>
2026-08-18 15:14:22 +0200

    Fix DeCaL initialization Fixes part of  issue  #427

--- dicee/models/clifford.py
+++ dicee/models/clifford.py
@@ -862,7 +862,8 @@ class DeCaL(BaseKGE):
     def __init__(self, args):
         super().__init__(args)
         self.name = 'DeCaL'
-        self.entity_embeddings = torch.nn.Embedding(self.num_entities, self.embedding_dim)
+        if not self.defer_large_embeddings:
+            self.entity_embeddings = torch.nn.Embedding(self.num_entities, self.embedding_dim)
         self.relation_embeddings = torch.nn.Embedding(self.num_relations, self.embedding_dim)
         self.p = self.args.get("p", 0)
         self.q = self.args.get("q", 0)
