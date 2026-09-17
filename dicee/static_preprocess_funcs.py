7b0954022a018e2bd36f0c006ddd614f50f78a3c
ashishtiwari03 <ashish@facts-interim.de>
2026-09-17 21:34:11 +0200

    fix: allow partial config validation

--- dicee/static_preprocess_funcs.py
+++ dicee/static_preprocess_funcs.py
@@ -39,7 +39,7 @@ def timeit(func):
 
 def preprocesses_input_args(args):
     """ Sanity Checking in input arguments """
-    if args.trainer == "torchFSDP" and args.auto_batch_finding:
+    if getattr(args, "trainer", None) == "torchFSDP" and getattr(args, "auto_batch_finding", False):
         raise ValueError("--auto_batch_finding is not supported with --trainer torchFSDP; set --batch_size explicitly")
     FilteredRanker(**evaluation_tie_options(args))
     # To update the default value of Trainer in pytorch-lightnings
